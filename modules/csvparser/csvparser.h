3e37308b4a094c331d61f3f37cc94518f7692f82
Benke Tibor <ihrwein@gmail.com>
2015-01-19 17:01:00 +0100

    csvparser: wip

--- modules/csvparser/csvparser.h
+++ modules/csvparser/csvparser.h
@@ -27,16 +27,6 @@
 #include "parser/parser-expr.h"
 #include "csvparser-unescaped.h"
 
-#define LOG_CSV_PARSER_ESCAPE_NONE        0x0001
-#define LOG_CSV_PARSER_ESCAPE_BACKSLASH   0x0002
-#define LOG_CSV_PARSER_ESCAPE_DOUBLE_CHAR 0x0004
-#define LOG_CSV_PARSER_ESCAPE_MASK        0x0007
-#define LOG_CSV_PARSER_STRIP_WHITESPACE   0x0008
-#define LOG_CSV_PARSER_GREEDY             0x0010
-#define LOG_CSV_PARSER_DROP_INVALID       0x0020
-
-#define LOG_CSV_PARSER_FLAGS_DEFAULT  ((LOG_CSV_PARSER_STRIP_WHITESPACE) | (LOG_CSV_PARSER_ESCAPE_NONE))
-
 void log_csv_parser_set_flags(LogColumnParser *s, guint32 flags);
 void log_csv_parser_set_delimiters(LogColumnParser *s, const gchar *delimiters);
 void log_csv_parser_set_quotes(LogColumnParser *s, const gchar *quotes);
