05e413fa36b588c8bc491e30293993e9a1a9aeca
Tibor Benke <tibor.benke@balabit.com>
2016-03-10 21:34:16 +0100

    cmake: fix systemd-journal module
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- cmake/Modules/Findsystemd.cmake
+++ cmake/Modules/Findsystemd.cmake
@@ -2,7 +2,7 @@ include(LibFindMacros)
 
 libfind_pkg_check_modules(Libsystemd_PKGCONF libsystemd)
 
-libfind_pkg_detect(Libsystemd systemd FIND_PATH sd-daemon.h PATH_SUFFIXES systemd FIND_LIBRARY systemd)
+libfind_pkg_detect(Libsystemd libsystemd FIND_PATH sd-daemon.h PATH_SUFFIXES systemd FIND_LIBRARY systemd)
 set(Libsystemd_PROCESS_INCLUDES Libsystemd_INCLUDE_DIR)
 set(Libsystemd_PROCESS_LIBS Libsystemd_LIBRARY)
 libfind_process(Libsystemd)
